fd7b483d5169dae7f3b75ba5c52e33b406e6eb50
arade <werkasiwucha@gmail.com>
2019-08-10 14:08:14 +0200

    Jest Ok

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-                int multiplication=5;
+                int multiplication=1;
         int j=10;
         System .out.println(multiplication*=j);
-                double i=5;
-        double j=10;
-        double i*j;
-                System.out.print("i*j");
+                double i=1;
+
+        System.out.print("i*j");
 
     }
 }
