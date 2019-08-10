1ca3eaacc6c4f669c0f97f761a048232ea0c556e
arade <werkasiwucha@gmail.com>
2019-08-10 14:31:35 +0200

    add example

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Substraction.substract(5,10));
+        System.out.println(Substraction.substract(5, 10));
 
-        System.out.println(Division.division(23,12));
+        System.out.println(Division.division(23, 12));
 
-        System.out.println(Power.mathPower(20,-1));
+        System.out.println(Power.mathPower(20, -1));
 
-                int multiplication=2;
-        int j=10;
-        System .out.println(multiplication*=j);
-                double i=1;
-
-        System.out.print("i*j");
-
+        System.out.println(Multiplay.Multiplay(7,8));
     }
 }
+
