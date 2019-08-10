dde8fa634cbc69944064aa3f59b13e2b2fdeb93c
arade <werkasiwucha@gmail.com>
2019-08-10 13:42:08 +0200

    Coś nie dziala tylko nie wiem co :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+                int multiplication=5;
+        int j=10;
+        System .out.println(multiplication*=j);
+                double i=5;
+        double j=10;
+        double i*j
+                System.out.print("i*j");
+
     }
 }
