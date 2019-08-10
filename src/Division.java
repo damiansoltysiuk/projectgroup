2b72728962825d21fadca3c41966cd8eeb757064
Damian Sołtysiuk <damian.soltysiuk@gmail.com>
2019-08-10 14:03:55 +0200

    fix exception & method

--- src/Division.java
+++ src/Division.java
@@ -1,19 +1,10 @@
 public class Division {
-    private double a;
-    private double b;
-
-    public Division(double a, double b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public double division() {
-        double div = -1;
-        try {
-            div = this.a / this.b;
-        } catch (IllegalArgumentException e) {
-            System.out.println("Not try divided by 0!" + e);
+    public static double division(double a, double b) {
+        try{
+            return a / b;
+        } catch (Exception e) {
+            System.out.println("Not divided to 0!");
         }
-        return div;
+        return -1;
     }
 }
